43bdcc46c64ef04759be26cac5396b67c11d8467
beatlesm <rj.geng@gmail.com>
2022-02-22 17:43:39 -0800

    Task2: 2D - Secrets File

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -6,6 +6,11 @@
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
  */
-module.exports = {
+  require('dotenv').config() 
 
-}
+  module.exports = {      
+    PORT: process.env.PORT || 9000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,    
+    JWT_SECRET: process.env.JWT_SECRET || 'keep it secret!!!!!'
+  }
